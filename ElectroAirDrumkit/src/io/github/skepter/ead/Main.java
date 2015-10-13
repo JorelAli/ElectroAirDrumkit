87797cc8d19823bf13574d900a0e3334b487fd92
Skepter <skepter.misc@gmail.com>
2015-10-13 20:14:20 +0100

    Adds basic GUI
    See https://github.com/benwaffle/Leap-Motion-Instrument

--- ElectroAirDrumkit/src/io/github/skepter/ead/Main.java
+++ ElectroAirDrumkit/src/io/github/skepter/ead/Main.java
@@ -2,14 +2,19 @@ package io.github.skepter.ead;
 
 import io.github.skepter.ead.Utilities.SoundType;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RadialGradientPaint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 import com.leapmotion.leap.Controller;
 
@@ -21,6 +26,7 @@ public class Main {
 	JComboBox<SoundType> farLeftDrum = new JComboBox<SoundType>();
 	JComboBox<SoundType> rightDrum = new JComboBox<SoundType>();
 	JComboBox<SoundType> farRightDrum = new JComboBox<SoundType>();
+	public LeapVisualizerPanel panel = new LeapVisualizerPanel();
 
 	/**
 	 * Launch the application.
@@ -51,7 +57,7 @@ public class Main {
 	private void initialize() {
 		instance = this;
 		frame = new JFrame();
-		frame.setBounds(100, 100, 491, 429);
+		frame.setBounds(100, 100, 603, 595);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		leftDrum.addActionListener(new ActionListener() {
@@ -82,22 +88,80 @@ public class Main {
 				drum.addItem(type);
 			}
 		}
+		
 
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(
-				groupLayout.createSequentialGroup().addGap(27).addComponent(farLeftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE).addGap(60)
-						.addComponent(leftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE).addPreferredGap(ComponentPlacement.RELATED, 166, Short.MAX_VALUE)
-						.addComponent(rightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE).addGap(65)
-						.addComponent(farRightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE).addGap(45)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(
-				groupLayout
-						.createSequentialGroup()
-						.addGap(173)
-						.addGroup(
-								groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(leftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(rightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(farLeftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(farRightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)).addContainerGap(197, Short.MAX_VALUE)));
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(27)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(farLeftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(60)
+							.addComponent(leftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(146)
+							.addComponent(rightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(58)
+							.addComponent(farRightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(173)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(leftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(farLeftDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(rightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(farRightDrum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 317, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(28, Short.MAX_VALUE))
+		);
 		frame.getContentPane().setLayout(groupLayout);
+	}
+}
+
+class LeapVisualizerPanel extends JComponent{								//component for drawing stuff
+	private static final long serialVersionUID = 1337L;
+	int fingerX, fingerY;													//x and y coords for circle
+
+	public LeapVisualizerPanel() {
+	}
+
+	@Override
+	public void paintComponent(Graphics g){
+		Graphics2D g2d = (Graphics2D)g;
+
+		g2d.setColor(Color.white);
+		g2d.fillRect(0, 0, 800, 520);
+
+		Point2D center = new Point2D.Float(20,20);
+		float radius = 40;
+		float[] distribution = {0.0f, 1.0f};
+		float[] hsbcolor = Color.RGBtoHSB(255, 220, 178, null);
+		float[] hsbcolor2 = Color.RGBtoHSB(231, 158, 109, null);
+		Color[] colors = {Color.getHSBColor(hsbcolor[0], hsbcolor[1], hsbcolor[2]),Color.getHSBColor(hsbcolor2[0], hsbcolor2[1], hsbcolor2[2])};
+		RadialGradientPaint gradient = new RadialGradientPaint(center,radius,distribution,colors);
+
+		if(!(fingerX==0 && fingerY==0)){
+			g2d.setPaint(gradient);
+			g2d.fillOval(fingerX, fingerY, 40, 40);
+		}
+	}
+
+	public int getFingerX() {
+		return fingerX;
+	}
+	public void setFingerX(int fingerX) {
+		this.fingerX = fingerX;
+	}
+	public int getFingerY() {
+		return fingerY;
+	}
+	public void setFingerY(int fingerY) {
+		this.fingerY = fingerY;
 	}
 }
--- ElectroAirDrumkit/src/io/github/skepter/ead/Sample.java
+++ ElectroAirDrumkit/src/io/github/skepter/ead/Sample.java
@@ -1,11 +1,12 @@
 package io.github.skepter.ead;
+
 /******************************************************************************\
-* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
-* Leap Motion proprietary and confidential. Not for distribution.              *
-* Use subject to the terms of the Leap Motion SDK Agreement available at       *
-* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
-* between Leap Motion and you, your company or other organization.             *
-\******************************************************************************/
+ * Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+ * Leap Motion proprietary and confidential. Not for distribution.              *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at       *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+ * between Leap Motion and you, your company or other organization.             *
+ \******************************************************************************/
 
 import io.github.skepter.ead.Utilities.SoundType;
 
@@ -20,88 +21,98 @@ import com.leapmotion.leap.Listener;
 import com.leapmotion.leap.SwipeGesture;
 
 class SampleListener extends Listener {
-	
-    public void onInit(Controller controller) {
-        System.out.println("Initialized");
 
-    }
+	public void onInit(Controller controller) {
+		System.out.println("Initialized");
 
-    public void onConnect(Controller controller) {
-        System.out.println("Connected");
-        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
-        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
-        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
-        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
-    }
+	}
 
-    public void onDisconnect(Controller controller) {
-        //Note: not dispatched when running in a debugger.
-        System.out.println("Disconnected");
-    }
+	public void onConnect(Controller controller) {
+		System.out.println("Connected");
+		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+		controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+		controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+	}
 
-    public void onExit(Controller controller) {
-        System.out.println("Exited");
-    }
+	public void onDisconnect(Controller controller) {
+		// Note: not dispatched when running in a debugger.
+		System.out.println("Disconnected");
+	}
 
-    public void onFrame(Controller controller) {
-        // Get the most recent frame and report some basic information
-        Frame frame = controller.frame();
+	public void onExit(Controller controller) {
+		System.out.println("Exited");
+	}
 
-        GestureList gestures = frame.gestures();
-        for (int i = 0; i < gestures.count(); i++) {
-            Gesture gesture = gestures.get(i);
-            switch (gesture.type()) {
-                case TYPE_SWIPE:
-                    SwipeGesture swipe = new SwipeGesture(gesture);
-                    
-//                    for(Hand hand : swipe.frame().hands()){
-                    	
-                    	if(swipe.state() == State.STATE_STOP) {
-                    		if(swipe.direction().getY() < 0){
-//                        	SwipeGesture previousUpdate = new SwipeGesture(controller.frame(1).gesture(swipe.id()));
-//                        	if(previousUpdate.position().getY() > swipe.position().getY()) {
-                        		if(swipe.position().getX() > 125) {
-                        			Utilities.playSound((SoundType) Main.instance.farRightDrum.getSelectedItem());
-                        		} else if (swipe.position().getX() < 125 && swipe.position().getX() > 0 ){
-                        			Utilities.playSound((SoundType) Main.instance.rightDrum.getSelectedItem());
-                        		} else if (swipe.position().getX() < -125){
-                        			Utilities.playSound((SoundType) Main.instance.farLeftDrum.getSelectedItem());
-                        		} else if (swipe.position().getX() > -125 && swipe.position().getX() < 0 ){
-                        			Utilities.playSound((SoundType) Main.instance.leftDrum.getSelectedItem());
-                        		}
-                        	}
-                    }
-                default:
-//                    System.out.println("Unknown gesture type.");
-                    break;
-            }
-        }
+	public long map(long x, long in_min, long in_max, long out_min, long out_max) {
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	}
 
-        if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
-//            System.out.println();
-        }
-    }
+	public void onFrame(Controller controller) {
+		// Get the most recent frame and report some basic information
+		Frame frame = controller.frame();
+
+		Main.instance.panel.repaint();
+		Main.instance.panel.setFingerX((int) map((long) frame.fingers().get(0).tipPosition().getX(), -300, 300, 0, 800));
+		Main.instance.panel.setFingerY(520 - (int) map((long) frame.fingers().get(0).tipPosition().getY(), 20, 250, 0, 520));
+
+		GestureList gestures = frame.gestures();
+		for (int i = 0; i < gestures.count(); i++) {
+			Gesture gesture = gestures.get(i);
+			switch (gesture.type()) {
+				case TYPE_SWIPE:
+					SwipeGesture swipe = new SwipeGesture(gesture);
+
+					// for(Hand hand : swipe.frame().hands()){
+
+					if (swipe.state() == State.STATE_STOP) {
+						if (swipe.direction().getY() < 0) {
+							// SwipeGesture previousUpdate = new
+							// SwipeGesture(controller.frame(1).gesture(swipe.id()));
+							// if(previousUpdate.position().getY() >
+							// swipe.position().getY()) {
+							if (swipe.position().getX() > 125) {
+								Utilities.playSound((SoundType) Main.instance.farRightDrum.getSelectedItem());
+							} else if (swipe.position().getX() < 125 && swipe.position().getX() > 0) {
+								Utilities.playSound((SoundType) Main.instance.rightDrum.getSelectedItem());
+							} else if (swipe.position().getX() < -125) {
+								Utilities.playSound((SoundType) Main.instance.farLeftDrum.getSelectedItem());
+							} else if (swipe.position().getX() > -125 && swipe.position().getX() < 0) {
+								Utilities.playSound((SoundType) Main.instance.leftDrum.getSelectedItem());
+							}
+						}
+					}
+				default:
+					// System.out.println("Unknown gesture type.");
+					break;
+			}
+		}
+
+		if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
+			// System.out.println();
+		}
+	}
 }
 
 class Sample {
-	
+
 	public Sample() {
 		// Create a sample listener and controller
-        SampleListener listener = new SampleListener();
-        Controller controller = new Controller();
+		SampleListener listener = new SampleListener();
+		Controller controller = new Controller();
 
-        // Have the sample listener receive events from the controller
-        controller.addListener(listener);
+		// Have the sample listener receive events from the controller
+		controller.addListener(listener);
 
-        // Keep this process running until Enter is pressed
-        System.out.println("Press Enter to quit...");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		// Keep this process running until Enter is pressed
+		System.out.println("Press Enter to quit...");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        // Remove the sample listener when done
-        controller.removeListener(listener);
+		// Remove the sample listener when done
+		controller.removeListener(listener);
 	}
 }
